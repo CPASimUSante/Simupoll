9a8366f49b156529ccfb2d14ca9d830465780d9a
Dechepy olivier <dechepy.olivier@chu-amiens.fr>
2016-02-14 20:54:00 +0100

    Finish period page management

--- Form/PeriodType.php
+++ Form/PeriodType.php
@@ -5,49 +5,48 @@ namespace CPASimUSante\SimupollBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class PeriodType extends AbstractType
 {
-
-
-    private function setFieldOptions($options, $label)
-    {
-        return array(
-            'label' => $label,
-            'required' => true,
-            'read_only' => false,
-            'component' => true,
-            'autoclose' => true,
-            'input' => 'datetime',
-            'language' => $options['language'],
-            'format' => $this->translator->trans('date_form_format', array(), 'platform'),
-            'mapped' => false,
-        );
-    }
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $dateParams = array(
+            'format' => 'dd-MM-yyyy',
+            'widget' => 'single_text',
+            'input' => 'datetime',
+            'component' => true,
+            'attr' => array(
+                'class' => 'datepicker input-small',
+                'data-date-format' => 'dd-mm-yyyy',
+                'autocomplete' => 'off'
+            ),
+            'constraints' => array(new NotBlank())
+        );
+        $startParam = $dateParams;
+        $startParam['label'] = 'period_start';
+        $stopParam = $dateParams;
+        $stopParam['label'] = 'period_stop';
 
-        $generalOptions = array(
+       /* $generalOptions = array(
             'required' => true,
-            'read_only' => false,
+            'read_only' => true,
             'component' => true,
             'autoclose' => true,
             'input' => 'datetime',
             'language' => $options['language'],
-            'format' => $this->translator->trans('date_form_format', array(), 'platform'),
+            'format' => 'yyyy-MM-dd H:m:s',
             'mapped' => false,
         );
         $startOptions = $generalOptions;
-        $startOptions['label'] = 'period_start';
+        $startOptions['label'] = 'period_start';*/
         $builder
-            ->add('start', 'datepicker', $this->setFieldOptions($options, 'period_start'))
-            ->add('stop', 'datepicker', $this->setFieldOptions($options, 'period_stop'))
+            ->add('start', 'datepicker', $startParam)
+            ->add('stop', 'datepicker', $stopParam)
         ;
     }
     
